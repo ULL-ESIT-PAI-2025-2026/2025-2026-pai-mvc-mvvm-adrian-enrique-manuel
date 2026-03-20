b3108eddfbb2b8861da6c106f9e77d97931b1a2d
Enrique Gómez Díaz <149805807+EnriqueGD08@users.noreply.github.com>
2026-03-20 19:56:36 +0000

    ajuste de nombres

--- src/cabecera.ts
+++ src/cabecera.ts
@@ -4,9 +4,9 @@
  * Grado en Ingeniería Informática
  * Programación de Aplicaciones Interactivas 2025-2026
  *
- * @author Adrián Pérez Poleo
- * @author Enrique Gómez
- * @author Manuel Cadenas García
+ * @author Adrián Pérez Poleo (adrian.perez.46@ull.edu.es)
+ * @author Enrique Gómez Díaz (enrique.gomez.13@ull.edu.es)
+ * @author Manuel Cadenas García (manuel.cadenas.25@ull.edu.es)
  * @since Mar 20 2026
  * @desc Texto esplicativo
  * @see {@link si-es-necesario}
