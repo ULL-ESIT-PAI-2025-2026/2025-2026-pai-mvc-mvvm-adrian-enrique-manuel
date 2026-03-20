cfb2b962ee67518efd784dc72ef7a38b18daa82d
Enrique Gómez Díaz <149805807+EnriqueGD08@users.noreply.github.com>
2026-03-20 19:12:07 +0000

    Cabecera común para todos los archivos de código

---
+++ src/cabecera.ts
@@ -0,0 +1,13 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas 2025-2026
+ *
+ * @author Adrián Pérez Poleo
+ * @author Enrique Gómez
+ * @author Manuel Cadenas García
+ * @since Mar 20 2026
+ * @desc Texto esplicativo
+ * @see {@link si-es-necesario}
+ */
